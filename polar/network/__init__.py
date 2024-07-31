c09978f338e2f121abd408f0d30b2e0df23620e9
vedrenne <vedrenneluc@gmail.com>
2024-07-31 11:00:40 +0200

    add new weights

--- polar/network/__init__.py
+++ polar/network/__init__.py
@@ -4,5 +4,5 @@ from .model import PointNetAE
 
 here = Path(__file__).resolve().parent
 
-AE_BASELINE_NAME = 'logs.pt'  # 'JSCTR_V4.pt'
+AE_BASELINE_NAME = 'final.pt'
 AE_BASELINE_WEIGHTS = here / 'weights' / AE_BASELINE_NAME
